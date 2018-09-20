d58980441c2149fe642a59edff7bd932048a8852
Lior Vichmann <34236746+lior94@users.noreply.github.com>
2018-09-20 14:36:59 +0300

    Add files via upload

---
+++ search.js
@@ -0,0 +1,22 @@
+search = function(){
+const testFolder = './';
+const fs = require('fs');
+var flag=0;
+fs.readdir(testFolder, (err, files) => {
+  files.forEach(file => {
+    if(file.endsWith('.'+process.argv[2])){
+        let data=fs.readFileSync(file, {encoding: 'utf8'});
+        if(data.search(process.argv[3])!=-1){
+        console.log("C:\\MyFolder\\"+file);
+        flag=1;
+        }
+    }
+})
+if(!flag){
+    console.log("no file was found");
+};
+
+})
+}
+
+search();
